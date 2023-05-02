0044f662ef6b598ca7a09fdc3f3861adcfa18b60
Pedro Ertal <pedro.ertal@yahoo.com.br>
2023-05-02 09:11:40 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import HomePage from './src/pages/HomePage'
+
+
+const App = () => {
+
+  return (
+
+    <HomePage />
+
+
+  );
+
+}
+
+export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const HomePage = () => {
+
+  return (
+
+    <View>
+      <Text> PUC Minas </Text>
+    </View>
+
+
+  );
+
+}
+
+export default HomePage;
